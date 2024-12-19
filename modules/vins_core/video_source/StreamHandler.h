154017848a46fc9fe77dfec26af26bb856310424
apin <a_pintya@mail.ru>
2024-12-19 18:39:59 +0300

    added StreamHandler for GStreamer pipelines

---
+++ modules/vins_core/video_source/StreamHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by apin on 19.12.2024.
+//
+
+#ifndef VINS_FOCUS_STREAMHANDLER_H
+#define VINS_FOCUS_STREAMHANDLER_H
+
+
+class StreamHandler {
+
+};
+
+
+#endif //VINS_FOCUS_STREAMHANDLER_H
---
+++ modules/vins_core/video_source/StreamHandler.hpp
@@ -0,0 +1,57 @@
+//
+// Created by apin on 19.12.2024.
+//
+
+#ifndef VINS_FOCUS_STREAMHANDLER_H
+#define VINS_FOCUS_STREAMHANDLER_H
+
+#include "VideoSource.hpp"
+
+
+namespace vins_core
+{
+    class StreamHandler : public VideoSource
+    {
+    public:
+        /*********Public fields*********/
+        cv::VideoCapture video;
+
+        std::string pipeline{""};
+
+        /*********Public methods*********/
+        StreamHandler(std::string config_path);
+
+        /**
+         * @brief Return DataPackage for given video source with available data for it
+         *
+         * @return DataPackageBase
+         */
+        DataPackageBase read() override;
+
+
+        /**
+         * @brief Print out in the terminal info about current video
+         *
+         */
+        void print_info() override;
+
+
+    protected:
+        /*********Protected fields*********/
+
+
+        /*********Protected methods*********/
+
+
+    private:
+        /*********Private fields*********/
+
+
+        /*********Private methods*********/
+
+
+    };
+
+}
+
+#endif //VINS_FOCUS_STREAMHANDLER_H
